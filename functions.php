399b431eaa6d2392e70283ac8c402562550eae35
Isaac Rossi <isaacjohnrossi@gmail.com>
2022-03-28 22:55:41 +1100

    set up sass

--- functions.php
+++ functions.php
@@ -141,6 +141,8 @@ function personalportfolio_scripts() {
 	wp_enqueue_style( 'personalportfolio-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'personalportfolio-style', 'rtl', 'replace' );
 
+	wp_enqueue_style('billowmagazine-custom', get_template_directory_uri() . '/css/app.css');
+
 	wp_enqueue_script( 'personalportfolio-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
