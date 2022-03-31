609a36ace4ec9e60a29e634206798eea9712a9ce
Isaac Rossi <isaacjohnrossi@gmail.com>
2022-03-31 20:24:51 +1100

    added and styled the hero section

--- index.php
+++ index.php
@@ -16,7 +16,11 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-   
+   <?php
+
+    get_template_part('template-parts/content-hero');
+
+	 ?>
 	 
 	</main><!-- #main -->
 
---
+++ template-parts/content-hero.php
@@ -0,0 +1,10 @@
+<section class="hero  container">
+
+ <h1 class="hero__text">
+  Welcome.</br>
+  <span class="reg">Im Isaac, A frontend developer dreaming of location independence.</span>
+ </h1>
+
+ <img class="hero__image" src="<?php bloginfo('template_url');?>/assets/me.jpg">
+
+</section>
