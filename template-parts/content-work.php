8c223645590a95ac5ad1a2a753b3b1b9c594fdfc
Isaac Rossi <isaacjohnrossi@gmail.com>
2022-04-20 21:06:35 +1000

    made it so projects can be added as via the wordpress cms

--- template-parts/content-work.php
+++ template-parts/content-work.php
@@ -4,27 +4,40 @@
     <span class="reg">Selected</span> Work
   </h2>
 
+  <?php
+      // organise our options into a data object
+    $args = array(
+      'posts_per_page' => 2,
+      'orderby' => 'date',
+    );
+    // a variable with our query and options
+    $query = new WP_Query( $args );
+    // do a loop with our new query code 
+    if ($query->have_posts()): while ($query->have_posts()): $query->the_post()
+  ?>
+
   <div class="selected-work__content">
 
     <div class="selected-work__image">
-      <img class="selected-work__billow-img" src="<?php bloginfo('template_url');?>/assets/billow-mag.jpg">
+      <img class="selected-work__billow-img" src="<?php the_field('image');?>">
       <div class="selected-work__caption">
-        <p class="selected-work__stack">Wordpress, Tachyons, HTML, CSS</p>
+        <p class="selected-work__stack"><?php the_field('stack'); ?></p>
       </div>
     </div>
 
     <div class="selected-work__details">
-      <h3 class="selected-work__project-name">Billow Magazine</h3>
+      <h3 class="selected-work__project-name"><?php the_title()?></h3>
       <p class="selected-work__project-bio">
-        Billow magazine is a travel and surf website that features stories about wicked experiences from around the globe. It is a custom theme built with wordpress and the advanced custom fields plugin. It gives the client complete control over the content and structure of the stories. 
+        <?php the_field('description'); ?> 
       </p>
       <div class="selected-work__buttons">
-      <a href="/"><div class="selected-work__live">Live Site</div></a>
-        <a class="selected-work__github" href="/"> <img src="<?php bloginfo('template_url');?>/assets/github.svg"></a>
+      <a href="<?php the_field('link'); ?>"><div class="selected-work__live">Live Site</div></a>
+        <a class="selected-work__github" href="<?php the_field('github'); ?>"> <img src="<?php bloginfo('template_url');?>/assets/github.svg"></a>
       </div>
     </div>
 
   </div>
 
+  <?php endwhile; endif; ?>  
 
 </section>
